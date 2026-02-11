312cf1fd748e27644a25e8e8a1499485788b2f99
Rachit Lohani <rachitlohani@yahoo.com>
2026-02-11 11:38:06 -0800

    more changes

---
+++ src/data/client-docs.ts
@@ -0,0 +1,17 @@
+export const clientDocs: { slug: string; title: string; docId: string }[] = [
+  {
+    slug: "briefing-1",
+    title: "Client Briefing 1",
+    docId: "1q6sObVC7cn527z3ljeE59QKo9-CBUlWnGwP7yL8zMTM"
+  },
+  {
+    slug: "briefing-2",
+    title: "Client Briefing 2",
+    docId: "10jpS11cbFz0vPFpFtFG5xNa1dLDY1A2BgKx5Xd6YnT0"
+  },
+  {
+    slug: "briefing-3",
+    title: "Client Briefing 3",
+    docId: "1o4DLRNWJHb5M_Efm2WIzgGhSXx7Xmn64YRYZBR29LYc"
+  }
+];
---
+++ src/data/media.ts
@@ -0,0 +1,37 @@
+export const mediaItems: { title: string; url: string; type: "youtube" }[] = [
+  {
+    title: "Interview",
+    url: "https://www.youtube.com/watch?v=bZHTx4Fw8fY",
+    type: "youtube"
+  },
+  {
+    title: "Interview",
+    url: "https://www.youtube.com/watch?v=3_-NxT1OQRE",
+    type: "youtube"
+  },
+  {
+    title: "Interview",
+    url: "https://www.youtube.com/watch?v=D57TCkK7EGM",
+    type: "youtube"
+  },
+  {
+    title: "Interview",
+    url: "https://www.youtube.com/watch?v=Y7K6rmkf64E",
+    type: "youtube"
+  },
+  {
+    title: "Interview",
+    url: "https://www.youtube.com/watch?v=3ZzNqOcgxQo",
+    type: "youtube"
+  },
+  {
+    title: "Interview",
+    url: "https://www.youtube.com/watch?v=TlA1-d1zUEE",
+    type: "youtube"
+  },
+  {
+    title: "Interview",
+    url: "https://www.youtube.com/watch?v=_4G9AGBiK5Q",
+    type: "youtube"
+  }
+];
