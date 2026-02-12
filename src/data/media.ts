567749f75d27962e89c488f12157150d5c75793a
Rachit Lohani <rachitlohani@yahoo.com>
2026-02-12 10:06:19 -0800

    Revise media item titles and descriptions
    Updated titles and descriptions for media items featuring Rachit Lohani to provide more context and detail.

--- src/data/media.ts
+++ src/data/media.ts
@@ -8,52 +8,52 @@ export type MediaItem = {
 
 export const mediaItems: MediaItem[] = [
   {
-    title: "Interview with Rachit Lohani",
+    title: "Rachit Lohani on Harnessing AI for Enhanced Decision-Making",
     url: "https://www.youtube.com/watch?v=bZHTx4Fw8fY",
     type: "youtube",
-    description: "Interview with Rachit Lohani.",
+    description: "Rachit shares his journey through the tech industry and his expertise in AI and SaaS. He discusses the importance of optimizing technology to drive business growth while balancing operational efficiency with product reliability. .",
     transcript: []
   },
   {
-    title: "Interview with Rachit Lohani",
+    title: "Rachit Lohani: Setting the Standard for Employee Recruitment and Retention",
     url: "https://www.youtube.com/watch?v=3_-NxT1OQRE",
     type: "youtube",
-    description: "Interview with Rachit Lohani.",
+    description: "Brandon Deer sat down with Rachit Lohani to have a conversation about his career trajectory, tackling complex challenges at different levels of scale, growing engineering teams, and leveraging AI to transform human capital management.",
     transcript: []
   },
   {
-    title: "Interview with Rachit Lohani",
+    title: "Rachit Lohani Executive Leader | Thinker | Advisor",
     url: "https://www.youtube.com/watch?v=D57TCkK7EGM",
     type: "youtube",
-    description: "Interview with Rachit Lohani.",
+    description: "Always one to jump on the mic, Rachit Lohani loves to energize and inspire with his wealth of knowledge.",
     transcript: []
   },
   {
-    title: "Interview with Rachit Lohani",
+    title: "Rachit Lohani on Endor Labs",
     url: "https://www.youtube.com/watch?v=Y7K6rmkf64E",
     type: "youtube",
-    description: "Interview with Rachit Lohani.",
+    description: "Rachit Lohani, CTO, Paylocity - Endor Labs",
     transcript: []
   },
   {
-    title: "Interview with Rachit Lohani",
+    title: "How To Be an Effective CTO | Rachit Lohani | Startup CTO Summit 2023",
     url: "https://www.youtube.com/watch?v=3ZzNqOcgxQo",
     type: "youtube",
-    description: "Interview with Rachit Lohani.",
+    description: "Rachit talks about roles of CTOs during different product stages and phases.",
     transcript: []
   },
   {
-    title: "Interview with Rachit Lohani",
+    title: "An R&D Conversation with Atlassian's Rachit Lohani | Hetz Ventures",
     url: "https://www.youtube.com/watch?v=TlA1-d1zUEE",
     type: "youtube",
-    description: "Interview with Rachit Lohani.",
+    description: "Atlassian's Head of Engineering Rachit Lohani gave us perspective on managing - and scaling - global R&D teams, the strategies and challenges of building out DevOps culture, how to work with the CISO side of large organizations, and more. Interview by Judah Taub, Managing Partner at Hetz Ventures",
     transcript: []
   },
   {
-    title: "Interview with Rachit Lohani",
+    title: "Rachit Lohani : Improve App Security Through Collaboration Between Engineering and Security",
     url: "https://www.youtube.com/watch?v=_4G9AGBiK5Q",
     type: "youtube",
-    description: "Interview with Rachit Lohani.",
+    description: "In this session from LeanAppSec Summer 2023, a panel of industry leaders discusses how to improve collaboration between security and engineering and how to share responsibility for securing applications. Panel: Stacy Hong (SVP Engineering, Okta), David Tsao (CISO, Instacart), Rachit Lohani (CTO, Paylocity), and Varun Badhwar (CEO, Endor Labs)",
     transcript: []
   }
 ];
